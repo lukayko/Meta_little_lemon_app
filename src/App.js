71a04dc010423c2fdf2d81d098312e19f6727fbe
lukayko <lukay.cervenka@gmail.com>
2023-04-04 19:33:55 +0200

    updated files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import Header from "./components/Header";
-import Main from "./components/Main";
 import HeroSection from "./components/HeroSection";
 import ChefSection from "./components/ChefSection";
 import Menu from "./components/Menu";
@@ -11,7 +10,6 @@ function App() {
   return (
     <>
       <Header />
-      <Main />
       <HeroSection />
       <ChefSection />
       <Menu />
