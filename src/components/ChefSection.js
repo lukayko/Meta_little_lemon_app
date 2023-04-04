565ac21779672808a4d7ca61c2001fcdebc65864
lukayko <lukay.cervenka@gmail.com>
2023-04-04 21:48:57 +0200

    heading, herosection are done, started working on chefsection

--- src/components/ChefSection.js
+++ src/components/ChefSection.js
@@ -1,7 +1,12 @@
 import React from "react";
+import ChefSectionCSS from "./ChefSection.module.css";
 
 function ChefSection() {
-  return <div>ChefSection</div>;
+  return (
+    <div className={ChefSectionCSS.wrapper}>
+      <div></div>
+    </div>
+  );
 }
 
 export default ChefSection;
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,11 +5,16 @@ import saladIMG from "../assets/greek_salad.jpg";
 function HeroSection() {
   return (
     <div className={HeroSectionCSS.wrapper}>
-      <div>
-        <h1>test</h1>
-      </div>
-      <div>
-        <img src={saladIMG} />
+      <div className={HeroSectionCSS.container}>
+        <div className={HeroSectionCSS.child1}>
+          <h1>Enjoy our bestsellers!</h1>
+          <div className={HeroSectionCSS.btn}>
+            <button>Order now</button>
+          </div>
+        </div>
+        <div className={HeroSectionCSS.child}>
+          <img src={saladIMG} />
+        </div>
       </div>
     </div>
   );
